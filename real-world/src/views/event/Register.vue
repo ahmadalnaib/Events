fad74b518ddc5cbbd697ef5bfed4142fe8bde271
ahmad.al-naib <alnaib888@gmail.com>
2024-07-22 10:50:14 +0200

    programmative navi

--- real-world/src/views/event/Register.vue
+++ real-world/src/views/event/Register.vue
@@ -1,7 +1,15 @@
 <script setup>
+import { useRouter } from 'vue-router';
 const props = defineProps(['event'])
+
+const router = useRouter()
+
+const register = () => {
+router.push({ name: 'EventDetails' })
+}
 </script>
 
 <template>
  <p>Register</p>
+ <button @click="register">Register</button>
 </template>
